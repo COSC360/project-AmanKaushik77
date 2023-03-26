ff03268a878ac404585af091e312a4696f3eaefa
AmanKaushik77 <100389449+AmanKaushik77@users.noreply.github.com>
2023-03-25 17:31:32 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
+<?php 
+include 'config.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/styles.css">
+    <title>The Penalty Box - Home</title>
     <script src="https://kit.fontawesome.com/d0c6f74c0e.js" crossorigin="anonymous"></script>
-</head>
-    <title>The Penalty Box - Home </title>
 </head>
 <body>
     <header>
@@ -17,9 +21,9 @@
             <input type="text" placeholder="Search">
             <hr>
             <ul>
-                <li><i class="fa-solid fa-house"></i><a href = "index.html"> Home</a></li>
+                <li><i class="fa-solid fa-house"></i><a href = "index.php"> Home</a></li>
                 <li><i class="fa-solid fa-user"></i><a href = "profile.html"> Profile</a></li>
-                <li><i class="fa-solid fa-fire"></i><a href = "trending.html"> Trending</a></li>
+                <li><i class="fa-solid fa-fire"></i><a href = "trending.php"> Trending</a></li>
                 <li><i class="fa-solid fa-right-to-bracket"></i><a href = "login.html"> Login</a></li>
             </ul>
         </nav>
@@ -32,76 +36,29 @@
             <thead>
               <tr>
                 <th>Votes</th>
-                <th>Title</th>
                 <th>Topic</th>
+                <th>Title</th>
                 <th>User</th>
                 <th>Dive In!</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>Messi vs Ronaldo debate</td>
-                <td>Debate who is the goat of football here!</td>
-                <td>PerryP</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>Does the NBA all-star sucks</td>
-                <td>This year shows how terrible the allstar game has been due to the lack of effort from players</td>
-                <td>JumpmanDan</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>The Flames can't seem to win in OT!</td>
-                <td>Don't get me wrong, I love the Flames but whenever we go into OT i just turn the TV off and wait for the loss IG post...</td>
-                <td>DirtyDangles5</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>Lebron James is offically the GOAT</td>
-                <td>Lebron just beat Kareems record... I dont want you old heads to say that MJ the goat no more</td>
-                <td>SportsFanatic</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>The NFL is scripted!</td>
-                <td>The NFL releases a script in the start of every year to help max revenue</td>
-                <td>UncleTravy</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>Will Chelsea get Neymar?</td>
-                <td>Recent news says that Chelsea owners and Neymar had a private meeting about the possible move!</td>
-                <td>LondonLad</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>TANK vs Garcia is finally official!</td>
-                <td>After so many years this fight is finally set! Tank gonna win easy!</td>
-                <td>PromoAlerts</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>Vancouver needs to tank for Bedard</td>
-                <td>Bedard literally matches the Canucks play style and grew up in Vancouver. The Vancouver boy needs to play for his home town team!</td>
-                <td>VancityManny</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
-              <tr>
-                <td><button class="upvote" onclick="upvoteForum(1)"> &uarr;</button> <button class="downvote" onclick="upvoteForum(1)">&darr;</button></td>
-                <td>Open for Business?</td>
-                <td>Is the happy couple happy due to their open marriage or is it just an act?</td>
-                <td>User1259232502</td>
-                <td><a href = #><i class="fa-solid fa-message"></i></a></td>
-              </tr>
+              <?php
+                $sql = "SELECT P.body, U.username, T.title FROM posts P JOIN users U ON U.user_id = P.user_id JOIN topics T ON P.topic_id = T.topic_id LIMIT 10";
+                $res = $pdo->query($sql);
+
+                while($row = $res->fetch()){
+                  echo "<tr>";
+                  echo "<td><button class='upvote' onclick='upvoteForum(1)'> &uarr;</button> <button class='downvote' onclick='upvoteForum(1)'>&darr;</button></td>";
+                  echo "<td> ".$row["title"]."</td>";
+                  echo "<td> ".$row["body"]."</td>";
+                  echo "<td> ".$row["username"]."</td>";
+                  echo "<td><a href = #><i class='fa-solid fa-message'></i></a></td>";
+                  echo "</tr>";
+                }
+                $pdo = null;
+                $res = null;
+              ?>
             </tbody>
           </table>
         </div>
