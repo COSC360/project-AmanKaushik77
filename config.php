4e72a42462f09f261ee9e6717b1885c2ad0b9617
AmanKaushik77 <100389449+AmanKaushik77@users.noreply.github.com>
2023-03-27 00:49:22 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Create connection
 try{
-   $connString = "mysql:host=localhost;dbname=test2";
-   $username = "root";
-   $password = "";
+   $connString = "mysql:host=cosc360.ok.ubc.ca;dbname=db_68952928";
+   $username = "68952928";
+   $password = "68952928";
    $pdo = new PDO($connString,$username,$password);
    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
