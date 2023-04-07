bb110904db368122ea9abe2eaafb3b3f249b03bb
AmanKaushik77 <100389449+AmanKaushik77@users.noreply.github.com>
2023-04-07 01:41:06 -0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -20,11 +20,21 @@ include 'config.php';
         $email = $_GET['email'];
     }
     try{
-        $sql = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-        $sql->execute([$un, $email, $pas]);
-        echo "<div class=\"welcome\"><h2>Welcome to the Pentalty Box, please click below and sign back using the username and password you created to start chatting!!".$un."</h2></div>";
-        echo "<div class=\"returnButton\"><a href=\"login.html\">Return to Login</a></div>";
-        
+        $sql2 = "SELECT username, email FROM users WHERE username=? OR email=?";
+        $res = $pdo->prepare($sql2);
+        $res->bindValue(1,$un);
+        $res->bindValue(2, $email);
+        $res->execute();
+
+        if( $row = $res->fetch()){
+            echo "Username or Email has already been used please try again";
+            echo "<a href=login.html> Try again</a>";
+        }else{
+            $sql = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+            $sql->execute([$un, $email, $pas]);
+            echo "<div class=\"welcome\"><h2>Welcome to the Pentalty Box, please click below and sign back using the username and password you created to start chatting!!".$un."</h2></div>";
+            echo "<div class=\"returnButton\"><a href=\"login.html\">Return to Login</a></div>";
+        }
 
     }catch(PDOException $e){
         die($e->getMessage());
@@ -33,6 +43,5 @@ include 'config.php';
 </body>
 
 </html>
-
 
 
